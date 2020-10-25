844bd16440f0290eb538b64a751606122e776129
MokkeMeguru <meguru.mokke@gmail.com>
2020-10-26 03:58:49 +0900

    load env integrant
    環境変数を読み込む + develop 環境 / prod 環境を切り替えるための env.clj
    を追加

--- project.clj
+++ project.clj
@@ -5,42 +5,47 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                                   ;; for handler
-                 [ring/ring-jetty-adapter "1.7.1"]
+                 [ring/ring-jetty-adapter "1.8.2"]
                  [metosin/reitit "0.5.10"]
                  [ring-cors "0.1.13"]
                  [ring-logger "1.0.1"]
-                 [com.fasterxml.jackson.core/jackson-core "2.10.0"] ;; required!!!
+                 [com.fasterxml.jackson.core/jackson-core "2.11.3"] ;; required!!!
 
                  ;; for security
                  [buddy/buddy-hashers "1.6.0"]
 
                  ;; for json
                  [clj-time "0.15.2"]
-                 [cheshire "5.9.0"]
+                 [cheshire "5.10.0"]
                  ;; to deal with  environment variables
-                 [environ "1.1.0"]
+                 [environ "1.2.0"]
 
                  ;; for integrant-repl
-                 [integrant "0.7.0"]
-                 [integrant/repl "0.3.1"]
+                 [integrant "0.8.0"]
+                 [integrant/repl "0.3.2"]
 
                  ;; for logging
-                 [com.taoensso/timbre "4.10.0"]
-                 [com.fzakaria/slf4j-timbre "0.3.14"]
+                 [com.taoensso/timbre "5.1.0"]
+                 [com.fzakaria/slf4j-timbre "0.3.20"]
 
                  ;; for database
-                 [honeysql "0.9.8"]
-                 [seancorfield/next.jdbc "1.0.9"]
-                 [hikari-cp "2.9.0"]
-                 [org.postgresql/postgresql "42.2.5"]
+                 [honeysql "1.0.444"]
+                 [seancorfield/next.jdbc "1.1.610"]
+                 [hikari-cp "2.13.0"]
+                 [org.postgresql/postgresql "42.2.18"]
                  ;; ;; for migration
                  [ragtime "0.8.0"]
 
                  ;; for others
-                 [camel-snake-kebab "0.4.0"]
+                 [camel-snake-kebab "0.4.2"]]
 
-                 [lein-ancient "0.6.15"]]
   :main ^:skip-aot picpage.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}}
+  :plugins [[lein-environ "1.1.0"]
+            [cider/cider-nrepl "0.25.4"]
+            [refactor-nrepl "2.5.0"]]
+  :repl-options
+  {:host "0.0.0.0"
+   :port 39998})
